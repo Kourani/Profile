9fee3e469092873b3a846c5fb2919bf6f8fe6e9a
Kourani12 <kourani@umich.edu>
2024-12-30 00:19:00 -0500

    about clean

--- react-app/src/components/About/index.js
+++ react-app/src/components/About/index.js
@@ -1,44 +1,10 @@
 
 import "./About.css";
 
-
-import * as help from "../../context/help"
-
-import { useModal } from "../../context/Modal";
-
-
-import React, { useEffect, useState, useRef } from "react"
-import { useDispatch, useSelector } from "react-redux"
-import { useHistory } from 'react-router-dom'
+import React from "react"
 
 
 export default function About(){
-
-    const dispatch = useDispatch()
-    const history = useHistory()
-    const ulRef = useRef();
-
-    const { closeModal } = useModal();
-    const [showMenu, setShowMenu] = useState(false);
-
-    const closeMenu = (e) => {
-        if (!ulRef.current.contains(e.target)) {
-          setShowMenu(false);
-        }
-    };
-
-
-    const userState = useSelector(state=>state.session)
-
-    console.log(userState, 'userState')
-
-
-    const handleSubmit = async (e) => {
-        e.preventDefault()
-
-        help.subscribe()
-    }
-
 
     return (
         <>
